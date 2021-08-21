372e63e0cdebbff5ab860d35c96493ff893a88b8
Emre <emredalc@gmail.com>
2021-08-21 17:27:31 +0300

    course controller added

--- src/main/java/dev/patika/gittigidiyorhomework02/controller/CourseController.java
+++ src/main/java/dev/patika/gittigidiyorhomework02/controller/CourseController.java
@@ -1,8 +1,17 @@
 package dev.patika.gittigidiyorhomework02.controller;
 
+import dev.patika.gittigidiyorhomework02.model.Course;
 import dev.patika.gittigidiyorhomework02.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
 public class CourseController {
 
     CourseService courseService;
@@ -10,6 +19,32 @@ public class CourseController {
     @Autowired
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
+    }
+
+    @GetMapping("/courses")
+    public ResponseEntity<List<Course>> findAll(){
+        return new ResponseEntity<>(courseService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/courses")
+    public Course saveCourse(@RequestBody Course course){
+        return courseService.save(course);
+    }
+
+    @GetMapping("/courses/{id}")
+    public ResponseEntity<Course> findCourseById(@PathVariable int id){
+        return new ResponseEntity<>(courseService.findById(id),HttpStatus.OK);
+    }
+
+    @PutMapping("/courses")
+    public Course updateCourse(@RequestBody Course course){
+        return courseService.update(course);
+    }
+
+    @DeleteMapping("/courses/{id}")
+    public String deleteCourseById(@PathVariable int id){
+        courseService.deleteById(id);
+        return "Course is deleted";
     }
 }
 
